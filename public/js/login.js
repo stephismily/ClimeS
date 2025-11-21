dcd11fff844dd14c85e77d86902aa5f005208600
STEPHINA SMILY C <stephismily@gmail.com>
2025-11-21 20:49:51 +0530

    debugginngggg

--- public/js/login.js
+++ public/js/login.js
@@ -9,7 +9,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
   msg.style.color = "#2b88c6";
 
   try {
-    const response = await fetch(" https://climes.onrender.com/auth/login", {
+    const response = await fetch("https://climes.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
